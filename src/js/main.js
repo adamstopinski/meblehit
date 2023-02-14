adcdc2660418a67a2cdba094d43ae558d411f6df
Adam S <stopinski.a@gmail.com>
2023-02-14 09:51:12 +0100

    removed pop up when right mouse clicking on website

--- src/js/main.js
+++ src/js/main.js
@@ -45,14 +45,7 @@ const handleCurrentYear = () => {
 	footerYear.innerText = year
 }
 
-// document.addEventListener('mousedown', function (event) {
-// 	if (event.button === 2) {
-// 		Swal.fire({
-// 			icon: 'error',
-// 			title: 'Wszystkie prawa zastrzeżone',
-// 		  })
-// 	}
-// })
+
 
 navBtn.addEventListener('click', handleNav)
 handleCurrentYear()
